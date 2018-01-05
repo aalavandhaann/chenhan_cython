9cf67d989c8b4823568c9a44f1124d2922696d30
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2018-01-05 13:29:11 -0500

    Adding PYX and CPP files generated using Autowrap on a Linux environment

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-import os, pkg_resources
+import os, platform, pkg_resources
 from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 
 data_dir = pkg_resources.resource_filename("autowrap", "data_files")
 include_dir = os.path.join(data_dir, "autowrap")
 
-ext = Extension("py_chenhan",
-                sources = ['py_chenhan.cpp'],
+CPP_suffix = "_GCC.cpp";
+PYX_suffix = "_GCC.pyx";
+
+if(platform.system() == 'WINDOWS'):
+    CPP_suffix = "_WIN.cpp";
+    PYX_suffix = "_WIN.pyx";
+
+print('FOR THE DETECTED PLATFORM USE THE FILES ::: ');
+print(CPP_suffix);
+print(PYX_suffix);
+
+ext = Extension("chenhancc",
+                sources = ['chenhancc'+CPP_suffix],
                 language="c++",
                 extra_compile_args=["-std=c++14"],
                 extra_link_args=["-std=c++14"],
@@ -14,8 +25,11 @@ ext = Extension("py_chenhan",
                )
 
 setup(cmdclass={'build_ext':build_ext},
-      name="py_chenhan",
+      name="chenhancc",
       version="0.0.1",
-      ext_modules = [ext]
+      ext_modules = [ext],
+      install_requires=[
+          'autowrap',
+      ]
      )
 
