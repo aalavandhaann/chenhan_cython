31859d38bfebbc674b53a184dc722dd45bdfb691
AM25300 <AM25300@logti047895.ens.ad.etsmtl.ca>
2018-01-05 16:04:06 -0500

    Changes to the compiled files

--- setup.py
+++ setup.py
@@ -5,21 +5,8 @@ from Cython.Distutils import build_ext
 data_dir = pkg_resources.resource_filename("autowrap", "data_files")
 include_dir = os.path.join(data_dir, "autowrap")
 
-CPP_suffix = "_GCC.cpp";
-PYX_suffix = "_GCC.pyx";
-
-print('PLATFORM DETECTED ', platform.system());
-
-if(platform.system().upper() == 'WINDOWS'):
-    CPP_suffix = "_WIN.cpp";
-    PYX_suffix = "_WIN.pyx";
-
-print('FOR THE DETECTED PLATFORM USE THE FILES ::: ');
-print(CPP_suffix);
-print(PYX_suffix);
-
 ext = Extension("chenhancc",
-                sources = ['chenhancc'+CPP_suffix],
+                sources = ['chenhancc.pyx', 'chenhancc.cpp'],
                 language="c++",
                 extra_compile_args=["-std=c++14"],
                 extra_link_args=["-std=c++14"],
